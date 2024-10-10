ad95e561f9e32560335e051b9f2b659631848bc5
sbenjamin51 <benjamins@xavier.edu>
2024-10-10 13:57:17 -0400

    Added Files from XavierMapApp

---
+++ AdmissioniOS/BuildingsView/BuildingsDetail.swift
@@ -0,0 +1,27 @@
+//
+//  BuildingsDetail.swift
+//  XavierMapApp
+//
+//  Created by Simon Benjamin on 10/8/24.
+//
+/*
+import SwiftUI
+
+struct BuildingsDetail: View {
+    @Environment(ModelData.self) var modelData
+    var building: Building
+    var buildingIndex: Int {
+        modelData.building.firstIndex(where: { $0.id == building.id })!
+    }
+    
+    
+    var body: some View {
+        @Bindable var modelData = modelData
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    BuildingsDetail()
+}
+*/
---
+++ AdmissioniOS/MainView.swift
@@ -0,0 +1,49 @@
+//
+//  MainView.swift
+//  XavierMapApp
+//
+//  Created by Simon Benjamin on 9/27/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            MapView()
+                .tabItem() {
+                    Image(systemName: "map")
+                    Text("Map")
+                }
+            
+            RoutesView()
+                .tabItem() {
+                    Image(systemName: "point.bottomleft.forward.to.point.topright.scurvepath")
+                    Text("Routes")
+                }
+            
+            BuildingsView()
+                .tabItem() {
+                    Image(systemName: "building.columns.fill")
+                    Text("Buildings")
+                    
+                }
+            
+            MoreView()
+                .tabItem() {
+                    Image(systemName: "ellipsis.circle")
+                    Text("More")
+                    
+                }
+            
+            
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    MainView()
+}
