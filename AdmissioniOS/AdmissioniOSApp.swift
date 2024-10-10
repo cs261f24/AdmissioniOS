a772d967c1517d5f4530b8e229f54866f45d2ccf
sbenjamin51 <benjamins@xavier.edu>
2024-10-10 13:47:00 -0400

    Initial Commit

---
+++ AdmissioniOS/AdmissioniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmissioniOSApp.swift
+//  AdmissioniOS
+//
+//  Created by Simon Benjamin on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AdmissioniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdmissioniOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdmissioniOS
+//
+//  Created by Simon Benjamin on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
