64a487b0dd2b3e3318e9fe057370a6efcdfa3191
sbenjamin51 <benjamins@xavier.edu>
2024-10-10 13:57:41 -0400

    #2

---
+++ AdmissioniOS/BuildingsView.swift
@@ -0,0 +1,18 @@
+//
+//  BuildingsView.swift
+//  XavierMapApp
+//
+//  Created by Simon Benjamin on 9/27/24.
+//
+
+import SwiftUI
+
+struct BuildingsView: View {
+    var body: some View {
+        BuildingsList()
+    }
+}
+
+#Preview {
+    BuildingsView()
+}
---
+++ AdmissioniOS/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  XavierMapApp
+//
+//  Created by Simon Benjamin on 9/27/24.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}
