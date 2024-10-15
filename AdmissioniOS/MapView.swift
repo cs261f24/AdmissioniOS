0ab7a2eec9c09178d9cff1806e1f13e535384ef4
Evan Gunnulfsen <gunnulfsene@xavier.edu>
2024-10-15 13:22:57 -0400

    Implemented interactive markers

--- AdmissioniOS/MapView.swift
+++ AdmissioniOS/MapView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import MapKit
-import SwiftData 
+import SwiftData
 
 struct MapView: View {
     @State private var cameraPosition: MapCameraPosition = .region(
@@ -18,17 +18,24 @@ struct MapView: View {
     )
     
     @StateObject private var locationManager = LocationManager()
+    // state to control navigation
+    @State private var showDetail = false
 
     var body: some View {
-        Map(position: $cameraPosition, interactionModes: .all) {
-            // Display the user location annotation
-            UserAnnotation()
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
-            Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
+        // wrap Map in a navigation stack to support navigation between views
+        NavigationStack {
+            Map(position: $cameraPosition, interactionModes: .all) {
+                // Display the user location annotation
+                UserAnnotation()
+               // Wrap markers in naviagtion link for direct naviagtion
+                    // is currently giving an error saying that buidlingsdetail
+                    // isn't in scope. This is because BuildingsView hasn't been
+                    // defined yet
+                NavigationLink(destination: BuildingsDetail()) {
+                    Marker("Schott Hall", coordinate: CLLocationCoordinate2D(latitude: 39.14749516485998, longitude: -84.47589391133172))
+                }
+            }
+           
         }
         
         .onAppear {
@@ -87,12 +94,5 @@ struct ContentView_Previews: PreviewProvider {
         MapView()
     }
 }
-
-
-
-
-
-
-
 
 
