f3ab5231c724e4be012edcb3b316552e5b2cf8de
Eric Alley <alleye@xavier.edu>
2024-10-18 14:36:00 -0400

    Filtered markers and changed coordinate in JSON

---
+++ AdmissioniOS/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  AdmissioniOS
+//
+//  Created by Simon Benjamin on 10/17/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
+
